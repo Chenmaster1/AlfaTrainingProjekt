196083e2b1076bd39366eee61d792b6d661f1561
yovo-cgn <47471893+yovo-cgn@users.noreply.github.com>
2020-02-21 15:30:39 +0100

    Update AlfaTrainingProjekt.java

--- AlfaTrainingProjekt/src/alfatrainingprojekt/AlfaTrainingProjekt.java
+++ AlfaTrainingProjekt/src/alfatrainingprojekt/AlfaTrainingProjekt.java
@@ -4,6 +4,7 @@ public class AlfaTrainingProjekt {
 
     public static void main(String[] args) {
         System.out.println("Hallo Welt");
+          System.out.println("Hallo Wel2t");
     }
 
 }
