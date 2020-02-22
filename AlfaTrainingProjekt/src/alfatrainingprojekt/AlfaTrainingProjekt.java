12f82b9c6c7b9a9da49aad28696d0fd693ccfa7e
Nick <Nick@DESKTOP-BBR66C9>
2020-02-22 14:47:12 +0100

    Test

--- AlfaTrainingProjekt/src/alfatrainingprojekt/AlfaTrainingProjekt.java
+++ AlfaTrainingProjekt/src/alfatrainingprojekt/AlfaTrainingProjekt.java
@@ -3,8 +3,9 @@ package alfatrainingprojekt;
 public class AlfaTrainingProjekt {
 
     public static void main(String[] args) {
-        System.out.println("Hallo Welt");
-          System.out.println("Hallo Wel2t");
+    	System.out.println("Hallo Welt");
+     	System.out.println("Hallo Wel2t");
+     	System.out.println("Test");
     }
 
 }
