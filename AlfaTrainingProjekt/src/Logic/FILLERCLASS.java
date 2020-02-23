0f8651ccd34b2aae78c3bedcaba8bc2b26a72f64
Kevin <kcmasterchen@gmail.com>
2020-02-23 20:30:27 +0100

    Packages für tests und logic hinzugefügt Fillerklassen hinzugefügt. können gelöscht werden sobald andere Klasse hinzugefügt wurde

---
+++ AlfaTrainingProjekt/src/Logic/FILLERCLASS.java
@@ -0,0 +1,5 @@
+package Logic;
+
+public class FILLERCLASS {
+	//Nur für package, kann gelöscht werden, sobald eine andere hinzugefügt wurde
+}
---
+++ AlfaTrainingProjekt/src/Tests/FILLERCLASS.java
@@ -0,0 +1,5 @@
+package Tests;
+
+public class FILLERCLASS {
+	//Nur für package, kann gelöscht werden, sobald eine andere hinzugefügt wurde
+}
