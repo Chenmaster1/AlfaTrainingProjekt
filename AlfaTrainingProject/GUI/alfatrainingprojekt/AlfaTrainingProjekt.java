ce6228e411a570d1a81c97306ba2f9efa5acb97d
Alfa <Alfa@HB-HOME-003>
2020-02-27 13:40:35 +0100

    anpassungen

--- AlfaTrainingProject/GUI/alfatrainingprojekt/AlfaTrainingProjekt.java
+++ AlfaTrainingProject/GUI/alfatrainingprojekt/AlfaTrainingProjekt.java
@@ -1,9 +1,10 @@
 package alfatrainingprojekt;
 
 import GUI.MyFrame;
+import java.util.ArrayList;
+import javax.swing.JPanel;
 
 public class AlfaTrainingProjekt {
-
     public static void main(String[] args) {
     	MyFrame frame = new MyFrame();
     	frame.start();
