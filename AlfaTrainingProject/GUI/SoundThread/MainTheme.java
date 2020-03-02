863a0cb34e1b6d0e105dec3ca4ff4ac842611164
Kevin <Kevin@DESKTOP-BBR66C9>
2020-03-02 11:41:49 +0100

    sound angepasst

--- AlfaTrainingProject/GUI/SoundThread/MainTheme.java
+++ AlfaTrainingProject/GUI/SoundThread/MainTheme.java
@@ -17,7 +17,7 @@ public class MainTheme implements Runnable{
 	public void run() {
 		//InputStream is = getClass().getClassLoader().getResourceAsStream("Sounds/MainSound.mp4");
 		try {
-			File file = new File("Sounds/MainSound.wav");
+			File file = new File(getClass().getClassLoader().getResource("MainSound.wav").getFile());
 			AudioInputStream stream = AudioSystem.getAudioInputStream(file);
 			AudioFormat format = stream.getFormat();
 			DataLine.Info info = new DataLine.Info(Clip.class, format);
@@ -29,5 +29,5 @@ public class MainTheme implements Runnable{
 		}
 	}
 
-	
+
 }
