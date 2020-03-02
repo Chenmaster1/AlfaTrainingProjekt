67cb03f021c24c8e9871a59865216752e782660e
Kevin <Kevin@DESKTOP-BBR66C9>
2020-03-02 11:32:27 +0100

    mp4 zu wav umgewandelt und angepasst

--- AlfaTrainingProject/GUI/SoundThread/MainTheme.java
+++ AlfaTrainingProject/GUI/SoundThread/MainTheme.java
@@ -1,5 +1,6 @@
 package SoundThread;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -14,16 +15,17 @@ public class MainTheme implements Runnable{
 
 	@Override
 	public void run() {
-		InputStream is = getClass().getClassLoader().getResourceAsStream("Sounds/MainSound.mp4");
+		//InputStream is = getClass().getClassLoader().getResourceAsStream("Sounds/MainSound.mp4");
 		try {
-			AudioInputStream stream = AudioSystem.getAudioInputStream(is);
+			File file = new File("Sounds/MainSound.wav");
+			AudioInputStream stream = AudioSystem.getAudioInputStream(file);
 			AudioFormat format = stream.getFormat();
 			DataLine.Info info = new DataLine.Info(Clip.class, format);
 			Clip clip = (Clip) AudioSystem.getLine(info);
 			clip.open(stream);
 			clip.start();
 		}catch(Exception ex) {
-			System.out.println("sound klappt nicht: " + ex.getMessage());
+			System.out.println("sound klappt nicht: " + ex.getMessage()  );
 		}
 	}
 
