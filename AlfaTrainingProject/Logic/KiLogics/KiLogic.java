3a369dca5af4d66bac873344dec7772386a9f732
Kevin <Kevin@DESKTOP-BBR66C9>
2020-03-02 12:17:46 +0100

    KiLogic angepasst

--- AlfaTrainingProject/Logic/KiLogics/KiLogic.java
+++ AlfaTrainingProject/Logic/KiLogics/KiLogic.java
@@ -1,10 +1,12 @@
 package KiLogics;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import Actions.Action;
 import GameLogic.SingleplayerGame;
 import Heroes.Hero;
+import Hideouts.Hideout;
 
 /**
  * Diese Klasse dient als Grundgeruest fuer jede implementierte KI.
@@ -19,6 +21,51 @@ public abstract class KiLogic {
 	 * @param hero der aktuelle Held
 	 * @param singleplayerGame das aktuelle Spiel
 	 */
-	public abstract void chooseAction(ArrayList<Action> actions, Hero hero, SingleplayerGame singleplayerGame);
+	public abstract Action chooseAction(ArrayList<Action> actions, Hero hero, SingleplayerGame singleplayerGame);
+	
+	public int chooseAttackField(SingleplayerGame singleplayerGame){
+		
+		int ownPosition = -1;
+		for(Hideout hideout : singleplayerGame.getMap().getHideouts()) {
+			
+			if(singleplayerGame.getMap().getHideoutHero().containsKey(hideout)) {
+				
+				if(singleplayerGame.getMap().getHideoutHero().get(hideout).equals(singleplayerGame.getCurrentHero())) {
+					ownPosition = hideout.getFieldNumber();
+				}
+			}
+		}
+		
+		//es wird solange ein Hideout gewaehlt, bis ein passendes getroffen wird.
+		//solange mehr als 6 felder aktiv sind, wird er selber nicht getroffen bei der auswahl 
+		//und ein entsprechendes Feld wird gewaehlt
+		//sind weniger aktiv wird nur geschaut ob das Feld aktiv ist
+		
+		while(true) {
+			Random random = new Random();
+			int attackField = random.nextInt(21);
+			int activeFields = 0;
+
+			if(singleplayerGame.getMap().getHideouts().get(attackField).isActive()) {
+				
+				for(Hideout hideout : singleplayerGame.getMap().getHideouts()) {
+					if(hideout.isActive())
+						activeFields++;
+				}
+				
+				if(activeFields <= 7) {
+					if(attackField < ownPosition + 2 || attackField > ownPosition - 2) {
+						return attackField;
+					}
+				}else {
+					return attackField;
+				}
+			}															
+		}
+		//reduce sollte bereits in der action, bzw ability aufgerufen werde, sofern die ability einen Zug ersetzt
+		//singleplayerGame.reduceCurrentActionPoints();
+	}
+	
+	
 	
 }
--- AlfaTrainingProject/Logic/KiLogics/KiLogicTolpanLongbeard.java
+++ AlfaTrainingProject/Logic/KiLogics/KiLogicTolpanLongbeard.java
@@ -15,22 +15,24 @@ import Hideouts.Hideout;
 public class KiLogicTolpanLongbeard extends KiLogic {
 
 	@Override
-	public void chooseAction(ArrayList<Action> actions, Hero hero, SingleplayerGame singleplayerGame) {
+	public Action chooseAction(ArrayList<Action> actions, Hero hero, SingleplayerGame singleplayerGame) {
+		
+		Action resultAction = null;
 		
 		//es wird solange eine Action ausgefuehrt, wie Aktionspunkte uebrig sind
 		//Tolpan hat keine Faehigkeit die während seines Zuges eingesetzt wird
 		//seine Prioritaet liegt beim verstecken, dann verzoegerung abbauen und dann angreifen
 		while(singleplayerGame.getCurrentActionPoints() > 0) {
 			if(hero.isVisible()) {
-				/*for(Action action : actions) {
-				 * 	if(action instanceof ActionHide)
-				 * 		action.useAction(singleplayerGame);
-				 *}
-				 */
+				for(Action action : actions) {
+				  	//if(action instanceof ActionHide)
+					resultAction = action;
+				}
+				 
 			}else if(hero.getDelayTokens() > 0) {
 				for(Action action : actions) {
 					if(action instanceof ActionWorkOffDelay) {
-						action.useAction(singleplayerGame);
+						resultAction = action;
 					}
 				}
 			}else {
@@ -38,48 +40,18 @@ public class KiLogicTolpanLongbeard extends KiLogic {
 				 * TODO Angriff. Falls ein Held sichtbar ist, diesen angreifen. Ansonsten primaer Felder angreifen, 
 				 * bei denen man selber nicht getroffen wernden kann 
 				 */
-				int ownPosition = -1;
-				for(Hideout hideout : singleplayerGame.getMap().getHideouts()) {
-					
-					if(singleplayerGame.getMap().getHideoutHero().containsKey(hideout)) {
-						
-						if(singleplayerGame.getMap().getHideoutHero().get(hideout).equals(hero)) {
-							ownPosition = hideout.getFieldNumber();
-						}
+				
+				for(Action action : actions) {
+					if(action instanceof ActionAttack) {
+						resultAction = action;
 					}
 				}
 				
-				//es wird solange ein Hideout gewaehlt, bis ein passendes getroffen wird.
-				//solange mehr als 6 felder aktiv sind, wird er selber nicht getroffen bei der auswahl 
-				//und ein entsprechendes Feld wird gewaehlt
-				//sind weniger aktiv wird nur geschaut ob das Feld aktiv ist
-				while(true) {
-					Random random = new Random();
-					int attackField = random.nextInt(21);
-					int activeFields = 0;
-
-					if(singleplayerGame.getMap().getHideouts().get(attackField).isActive()) {
-						
-						for(Hideout hideout : singleplayerGame.getMap().getHideouts()) {
-							if(hideout.isActive())
-								activeFields++;
-						}
-						
-						if(activeFields <= 7) {
-							if(attackField < ownPosition + 2 || attackField > ownPosition - 2) {
-								attack(attackField);
-								break;
-							}
-						}else {
-							attack(attackField);
-							break;
-						}
-					}															
-				}	
 			}
-			//reduce sollte bereits in der action, bzw ability aufgerufen werde, sofern die ability einen Zug ersetzt
-			//singleplayerGame.reduceCurrentActionPoints();
+			
+			
 		}
+		return resultAction;
 	}
 
 	private void attack(int attackField) {
