80f523e3dc300f2fda1a485ebd3307d0d6472d35
Peter <Peter@peter-desktop.localdomain>
2020-03-03 21:57:48 +0100

    HideDice auf 6 Seiten, final static Variablen für die Ergebnisse

--- AlfaTrainingProject/Logic/Dice/HideDice.java
+++ AlfaTrainingProject/Logic/Dice/HideDice.java
@@ -9,6 +9,7 @@ import java.util.Random;
  */
 public class HideDice extends Dice
 {
+	public static final int RESULT_SUCCESS = 1, RESULT_FAILURE = 2, RESULT_NOTHING = 3;
 
     public HideDice(int countSides)
     {
@@ -23,33 +24,33 @@ public class HideDice extends Dice
 
         //rolling 6 sided dice (0-5)
         Random randomDice = new Random();
-        int rolledHideDice = randomDice.nextInt(5);
+        int rolledHideDice = randomDice.nextInt(countSides);
 
         int resultHideDice = 0;
         // resultHideDice = 0 >> no HideDice was rolled
-        // resultHideDice = 1 >> HideDice successfull       (rolled HideDice = 0 or 1)
+        // resultHideDice = 1 >> HideDice successful       (rolled HideDice = 0 or 1)
         // resultHideDice = 2 >> HideDice failure           (rolled HideDice = 2)       
         // resultHideDice = 3 >> HideDice nothing happens   (rolled HideDice = 3, 4 or 5)
 
         switch (rolledHideDice)
         {
             case 0:
-                resultHideDice = 1;
+                resultHideDice = RESULT_SUCCESS;
                 break;
             case 1:
-                resultHideDice = 1;
+                resultHideDice = RESULT_SUCCESS;
                 break;
             case 2:
-                resultHideDice = 2;
+                resultHideDice = RESULT_FAILURE;
                 break;
             case 3:
-                resultHideDice = 3;
+                resultHideDice = RESULT_NOTHING;
                 break;
             case 4:
-                resultHideDice = 3;
+                resultHideDice = RESULT_NOTHING;
                 break;
             case 5:
-                resultHideDice = 3;
+                resultHideDice = RESULT_NOTHING;
                 break;
 
         }
