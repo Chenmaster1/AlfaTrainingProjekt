e0a8891d86e06b7b406ed5c05c19e99badd183bf
Kevin <Kevin@DESKTOP-BBR66C9>
2020-03-09 15:21:00 +0100

    eclipse buildpath angepasst. LoginScreen pathes angepasst

--- AlfaTrainingProject/GUI/SoundThread/mp3test.java
+++ AlfaTrainingProject/GUI/SoundThread/mp3test.java
@@ -1,7 +1,7 @@
 package SoundThread;
 
 import jaco.mp3.player.MP3Player;
-import jaco.mp3.player.plaf.MP3PlayerUICompact;
+import jaco.mp3.player.MP3PlayerApplet;
 
 import java.io.File;
 import java.net.URL;
@@ -22,8 +22,10 @@ public class mp3test implements Runnable{
 		try {
 			player = new MP3Player();	
 		    player.setRepeat(true);   
-		    player.addToPlayList(getClass().getClassLoader().getResource("MainSound.mp4"));	
+		    player.add(getClass().getClassLoader().getResource("MainSound.mp4"));
 		    player.play();
+		    
+		    
 		}catch (Exception ex){
 			
 		}
