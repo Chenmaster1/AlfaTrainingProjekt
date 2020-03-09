53ba918f7a6cd11476a3ee69784c78aa91e8d78c
Yovo <yovo_cologne.de>
2020-03-09 11:03:47 +0100

    Dahlia KI

--- AlfaTrainingProject/Logic/KiLogics/KiLogicDahlia.java
+++ AlfaTrainingProject/Logic/KiLogics/KiLogicDahlia.java
@@ -10,8 +10,9 @@ import java.util.ArrayList;
 
 
 /**
- * Dahlia KiLogic Dahlia automaticaly hides once she is hit. While Dahlia has at
- * least 2 HP left, she´s a bit aggressive
+ * Dahlia KiLogic 
+ * Dahlia automaticaly hides once she is hit(passive ability). 
+ * While Dahlia has at least 2 HP left, she´s a bit aggressive
  *
  * @author Yovo
  */
@@ -21,67 +22,71 @@ public class KiLogicDahlia extends KiLogic
     @Override
     public Action chooseAction(ArrayList<Action> actions, Hero hero, SingleplayerGame singleplayerGame)
     {
-        Action returnAction = null, attackHideout = null, attackEnemy = null, removeDelay = null, seekHideout = null;
+        Action returnAction = null, attackSmth = null, removeDelay = null, seekHideout = null;
 
-        // number of visible enemys
+        // number of visible Heroes
         int numHeroesVisible = 0;
-    //TO-DO : check if enemys are visible
-      
-    
+        for (Hero h : singleplayerGame.getMap().getHeroes())
+        {
+            if (h.isVisible())
+            {
+                numHeroesVisible++;
+            }
+        }
+
+        // assigning actions Dahlias ability hideWhenHit is passive
+        for (Action a : actions)
+        {
+            if (a instanceof ActionAttack)
+            {
+                attackSmth = a;
+            }
+            else if (a instanceof ActionHide)
+            {
+                seekHideout = a;
+            }
+            else if (a instanceof ActionWorkOffDelay)
+            {
+                removeDelay = a;
+            }
+        }
+
         // if no hero or enemy is visible - attack hideouts far away
-        if(numHeroesVisible == 0 && singleplayerGame.getCurrentActionPoints() >0)
+        if (numHeroesVisible == 0)
         {
-            
-    //TO-DO : get own hideout position and select attackHideout far away
-                returnAction =  attackHideout;
-   
+            returnAction = attackSmth;
         }
-        
-        
-        // if only enemy is visible - attack enemy  
-         if (numHeroesVisible !=0 && !hero.isVisible() && singleplayerGame.getCurrentActionPoints() >0)
+
+        // if only enemys are visible - attack enemy  
+        if (numHeroesVisible != 0 && !hero.isVisible())
         {
-    //TO-DO : If more than 1 enemy - choose which enemy to attack
-            returnAction = attackEnemy;
+            returnAction = attackSmth;
         }
-        
-     
-         // 2+HP Dahlia - if Dahlia is visible and has at least 2 hitpoints
-          if (numHeroesVisible !=0 && hero.isVisible() && singleplayerGame.getCurrentActionPoints() >0 && hero.getCurrentHitPoints()>=2)
-          {
-              if (numHeroesVisible ==1)
-              {
-                returnAction =  attackHideout;
-              }
-              if (numHeroesVisible >=2)
-              {
-    //TO-DO : If more than 1 enemy - choose which enemy to attack              
-                returnAction =  attackEnemy;
-              }
-            
-          }
-          
-          // 1HP Dahlia - if Dahlia is visible and has 1 hitpoints
-          if (numHeroesVisible !=0 && hero.isVisible() && singleplayerGame.getCurrentActionPoints() >0 && hero.getCurrentHitPoints()==1)
-          {
-              if (hero.getDelayTokens()>0 && singleplayerGame.getCurrentActionPoints() >1)
-              {
-                returnAction =  removeDelay;
-              }
-              if (hero.getDelayTokens()>0 && singleplayerGame.getCurrentActionPoints() >1)
-              {
-                returnAction =  seekHideout;
-              }
-              
-              if (hero.getDelayTokens()>0 && singleplayerGame.getCurrentActionPoints() ==1)
-              {
-    //TO-DO : If more than 1 enemy - choose which enemy to attack              
-                returnAction =  attackEnemy;  
-              }
-            
-          }
-         
-         
+
+        // 2+HP Dahlia - if Dahlia is visible and has at least 2 hitpoints
+        if (numHeroesVisible != 0 && hero.isVisible() && hero.getCurrentHitPoints() >= 2)
+        {
+            returnAction = attackSmth;
+        }
+
+        // 1HP Dahlia - if Dahlia is visible and has 1 hitpoints
+        if (numHeroesVisible != 0 && hero.isVisible() && hero.getCurrentHitPoints() == 1)
+        {
+            if (hero.getDelayTokens() > 0 && singleplayerGame.getCurrentActionPoints() > 1)
+            {
+                returnAction = removeDelay;
+            }
+            if (hero.getDelayTokens() == 0 && singleplayerGame.getCurrentActionPoints() > 1)
+            {
+                returnAction = seekHideout;
+            }
+
+            if (hero.getDelayTokens() == 0 && singleplayerGame.getCurrentActionPoints() == 1)
+            {
+                returnAction = attackSmth;
+            }
+
+        }
 
         return returnAction;
     }
