a94328776f3cd83c58c1663a75ab6001a50a563c
Kevin <Kevin@DESKTOP-BBR66C9>
2020-03-09 14:33:19 +0100

    geändert

--- AlfaTrainingProject/GUI/SoundThread/mp3test.java
+++ AlfaTrainingProject/GUI/SoundThread/mp3test.java
@@ -14,11 +14,13 @@ import javax.swing.JFrame;
  */
 public class mp3test implements Runnable{
 
+	private static MP3Player player;
+	
 	@Override
 	public void run() {
 		
 		try {
-			MP3Player player = new MP3Player();	
+			player = new MP3Player();	
 		    player.setRepeat(true);   
 		    player.addToPlayList(getClass().getClassLoader().getResource("MainSound.mp4"));	
 		    player.play();
