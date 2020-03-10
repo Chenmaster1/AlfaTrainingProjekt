b7e4c43133399de02a302280c8acded3209ab9e7
Kevin <Kevin@DESKTOP-BBR66C9>
2020-03-10 09:18:24 +0100

    Balthur erstellt inklusive KiLogic. KiLogic angepasst. falls ki nicht overrided wird, wird die allgemeine logic benutzt

---
+++ AlfaTrainingProject/Logic/Heroes/HeroBalthur.java
@@ -0,0 +1,14 @@
+package Heroes;
+
+import KiLogics.KiLogic;
+import KiLogics.KiLogicBalthur;
+
+public class HeroBalthur extends Hero{
+
+	public HeroBalthur() {
+		super("Balthur", "BalthurDescription", "BalthurArtwork", 5, 3, (double) 2.5, new KiLogicBalthur(), "Hero_Card/Avatar_Balthur.jpg");
+		
+	}
+
+
+}
