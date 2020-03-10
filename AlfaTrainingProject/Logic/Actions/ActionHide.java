aae3facacf97645cf56ed3e433ac523cfbcdfc9f
Kevin <Kevin@DESKTOP-BBR66C9>
2020-03-10 09:03:01 +0100

    ActionHide fertig

--- AlfaTrainingProject/Logic/Actions/ActionHide.java
+++ AlfaTrainingProject/Logic/Actions/ActionHide.java
@@ -1,6 +1,12 @@
 package Actions;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import GameLogic.SingleplayerGame;
+import Heroes.Hero;
+import Hideouts.Hideout;
+import Maps.Map;
 
 public class ActionHide extends Action {
 
@@ -11,8 +17,47 @@ public class ActionHide extends Action {
 
 	@Override
 	public void useAction(SingleplayerGame singleplayerGame) {
-		// TODO Auto-generated method stub
-
+		hideHero(singleplayerGame.getCurrentHero(), singleplayerGame);
+	}
+	
+	/**
+	 * falls sich nicht der aktuelle Held versteckt, wird diese Methode aufgerufen
+	 * @param hero
+	 * @param singleplayerGame
+	 */
+	public void useAction(Hero hero, SingleplayerGame singleplayerGame) {
+		hideHero(hero, singleplayerGame);
 	}
 
+	private void hideHero(Hero hero, SingleplayerGame singleplayerGame) {
+		ArrayList<Hideout> availableHideouts = new ArrayList<Hideout>();
+		
+		HashMap<Hideout, Hero> hideoutHeroMap = singleplayerGame.getMap().getHideoutHero();
+		
+		for(Hideout hideout : singleplayerGame.getMap().getHideouts()) {
+			boolean used = false;
+			for(Hideout usedHideout : hideoutHeroMap.keySet()) {
+				if(hideout.getFieldNumber() == usedHideout.getFieldNumber()) {
+					used = true;
+					break;
+				}
+					
+			}
+			
+			if((!used) && hideout.isActive()) {
+				availableHideouts.add(hideout);
+			}
+		}
+		
+		int newHideout = (int) (Math.random() * availableHideouts.size());
+		
+		for(Hideout hideout : hideoutHeroMap.keySet()) {
+			if(hideoutHeroMap.get(hideout).equals(hero)) {
+				hideoutHeroMap.remove(hideout);
+				hideoutHeroMap.put(singleplayerGame.getMap().getHideouts().get(newHideout), hero);
+				hero.setVisible(false);
+			}
+		}
+	}
+	
 }
