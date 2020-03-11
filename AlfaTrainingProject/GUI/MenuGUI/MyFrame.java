a0c3bd94420264a097b8d57d0261c7072bc9cc33
Yovo <yovo_cologne.de>
2020-03-11 11:35:53 +0100

    MyFrame language selection, public static language,volume,path

--- AlfaTrainingProject/GUI/MenuGUI/MyFrame.java
+++ AlfaTrainingProject/GUI/MenuGUI/MyFrame.java
@@ -10,7 +10,6 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
-
 import Database.Database;
 import SoundThread.MainTheme;
 import java.io.BufferedReader;
@@ -20,71 +19,30 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+
 /**
  * Diese Klasse dient zum Starten des Programms und Aufbau des Grundfensters
- * 
+ *
  * @author Kevin
  *
  */
 @SuppressWarnings("serial")
-public class MyFrame extends JFrame{
-    
-        // get Settings (language / volume)
-        private File file;
-        private String path;
-        private String language = "german";
-        
-        
-        private static String chooseBundle = "/Bundle_DE";
-        
-        public static void setLanguage(String Bundle)
-        {
-            chooseBundle = Bundle;
-            System.out.println("myframe choose : " +chooseBundle);
-            
-        }
-                
-	public static ResourceBundle bundle = ResourceBundle.getBundle("LanguagePackages"+chooseBundle); //Bundle, bzw path aus der Datenbank holen
+public class MyFrame extends JFrame
+{
 
-	//-------------------------Panels-------------------------//
-	private JPanel panel;
+    /**
+     * global settings for volume, language get infos from
+     *  <code>HeroesOfTheArena\hota_setting.txt</code> if no file is found, use
+     * <code>public static String language = "/Bundle_DE", volume = "50"</code>
+     * filepath <code>public static String path;</xode> is prepared * @author
+     * Yovo
+     */
+    public static String path;
+    public static String language = "/Bundle_DE", volume = "50";
 
-	/**
-	 * Hier werden alle Componenten initialisiert. Zudem werden <code>initializeFrame()</code>
-	 * und <code>pack()</code> ausgeführt
-	 * 
-	 * @author Kevin
-	 */
-	public MyFrame () {
-            getSettings();
-            
-            
-		setUndecorated(true);
-		//System.out.println(bundle.getString("btnNew")); //Beispiel für Mehrsprachigkeit
-		
-		pack();
-	}
-	
-	//-------------------------public methods-------------------------//
-	/**
-	 * Dient zum Starten des Programms. Hier werden entsprechend Attribute 
-	 * und Der Frame auf Visible gesetzt.
-	 * 
-	 * @author Kevin
-	 */
-	public void start() {
-		setExtendedState(JFrame.MAXIMIZED_BOTH);
-		//panel = new MainFramePanel(this);
-		panel = new LoginPanel(this);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
-
-	}
-        
-        private void getSettings()
+    // get filepath
+    static
     {
-        //get setting from HeroesOfTheArena\hota_setting.txt
-        // get path
         try
         {
             Process p = Runtime.getRuntime().exec("reg query \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Folders\" /v personal");
@@ -97,46 +55,78 @@ public class MyFrame extends JFrame{
 
             path = new String(b);
             path = path.split("\\s\\s+")[4];
-            //final path:
             path += "\\HeroesOfTheArena";
-
         }
         catch (Throwable t)
         {
             t.printStackTrace();
         }
 
-        //get setting from HeroesOfTheArena\hota_setting.txt
         // read file and save to volume and language
         if (Files.exists(Paths.get(path + "\\hota_setting.txt")))
         {
             try
             {
                 BufferedReader br = new BufferedReader(new FileReader(path + "\\hota_setting.txt"));
-                String volume = br.readLine();
-                String language = br.readLine();
-                if (language == "german")
+                volume = br.readLine();
+                language = br.readLine();
+                if (language.equalsIgnoreCase("german"))
                 {
-                    setLanguage("/Bundle_DE");
-                   
+                    language = "/Bundle_DE";
                 }
-                if (language == "english")
+                if (language.equalsIgnoreCase("english"))
                 {
-                    setLanguage("/Bundle_EN");
-                   
+                    language = "/Bundle_EN";
                 }
-                
-                    
-                System.out.println("string language " + language);
-                                       
-                
             }
             catch (Exception e)
             {
                 e.printStackTrace();
+                language = "/Bundle_DE";
+                volume = "50";
             }
         }
+    }
+
+    // load language package 
+    public static ResourceBundle bundle = ResourceBundle.getBundle("LanguagePackages" + language); //Bundle, bzw path aus der Datenbank holen
+
+    //-------------------------Panels-------------------------//
+    private JPanel panel;
+
+
+    /**
+     * Hier werden alle Componenten initialisiert. Zudem werden
+     * <code>initializeFrame()</code> und <code>pack()</code> ausgeführt
+     *
+     * @author Kevin
+     */
+    public MyFrame()
+    {
+        setUndecorated(true);
+        //System.out.println(bundle.getString("btnNew")); //Beispiel für Mehrsprachigkeit
+
+        pack();
+    }
+
+    //-------------------------public methods-------------------------//
+
+    /**
+     * Dient zum Starten des Programms. Hier werden entsprechend Attribute und
+     * Der Frame auf Visible gesetzt.
+     *
+     * @author Kevin
+     */
+    public void start()
+    {
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        //panel = new MainFramePanel(this);
+        panel = new LoginPanel(this);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
 
     }
 
+
 }
+
