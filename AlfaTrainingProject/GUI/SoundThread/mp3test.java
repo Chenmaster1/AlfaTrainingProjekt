0cf284a1523ec3c1ed158b95a0ae501cc43b3b64
Yovo <yovo_cologne.de>
2020-03-13 15:13:51 +0100

    sound problem at start solved

--- AlfaTrainingProject/GUI/SoundThread/mp3test.java
+++ AlfaTrainingProject/GUI/SoundThread/mp3test.java
@@ -18,6 +18,7 @@ public class mp3test implements Runnable
     private static MP3Player player;
     private static int volume;
 
+
     @Override
     public void run()
     {
@@ -38,22 +39,36 @@ public class mp3test implements Runnable
     }
 
 
+    /**
+     * changes the volume of the mp3 player from SettingsPanel
+     *  @author Yovo
+     */
     public static void setVolume(int volume)
     {
-       
+
         player.setVolume(volume);
-      
-    }
-    
-     public static void setVolumInitialize(int volumeInitialice)
-    {
-       volume = volumeInitialice;
-        //player.setVolume(volume);
-      
+
     }
 
-    public static void stopPlayer() {
-    	player.stop();
+
+    /**
+     * used upon starting the Programm <br>
+     * in MyFrame reads volume from settings.txt and sets mp3 volume
+     * 
+     *  @author Yovo
+     */
+    public static void setVolumInitialize(int volumeInitialice)
+    {
+        volume = volumeInitialice;
+
     }
+
+
+    public static void stopPlayer()
+    {
+        player.stop();
+    }
+
+
 }
 
