95156333de5af152a079d67ee08ad640e38ede26
Peter <Peter@peter-desktop.localdomain>
2020-03-15 14:21:25 +0100

    Heldentext richtig anzeigen

--- AlfaTrainingProject/GUI/InGameGUI/HeroPanelLarge.java
+++ AlfaTrainingProject/GUI/InGameGUI/HeroPanelLarge.java
@@ -415,6 +415,8 @@ public class HeroPanelLarge extends JPanel {
 		removeAll();
 		initializeActionListPanel();
 		initializeTextFields();
+		updateActionListPanelBounds();
+		updateTextFieldsBounds();
 		repaint();
 	}
 
