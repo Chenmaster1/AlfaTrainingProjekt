83b0f75f07bc512bae33b5cedee07e59a21ead07
Peter <Peter@peter-desktop.localdomain>
2020-03-19 09:50:43 +0100

    Ränder des Log Panel bleiben konstant

--- AlfaTrainingProject/GUI/InGameGUI/LogHeroActionPanel.java
+++ AlfaTrainingProject/GUI/InGameGUI/LogHeroActionPanel.java
@@ -1,48 +1,53 @@
 package InGameGUI;
 
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
-public class LogHeroActionPanel extends JPanel
-{
-   private JLabel labelLogHeroAction;
-   private JTextArea textAreaLogHeroAction;
-   private JScrollPane scrollTextAreaLogHeroAction;
-    
-    public LogHeroActionPanel()
-    {
-        setLayout(null);
-       // labelLogHeroAction = new JLabel("logoLog");
-        //labelLogHeroAction.setBounds(5, 5, 150, 50);
-        
-        textAreaLogHeroAction = new JTextArea();
-        scrollTextAreaLogHeroAction = new JScrollPane(textAreaLogHeroAction);
-        
-        scrollTextAreaLogHeroAction.setBounds(5, 5, 170, 385);
-        
-        //test filler
-        String str = "the game begins \n";
+public class LogHeroActionPanel extends JPanel {
+	private JLabel labelLogHeroAction;
+	private JTextArea textAreaLogHeroAction;
+	private JScrollPane scrollTextAreaLogHeroAction;
 
-        textAreaLogHeroAction.setText(str);
-        
-        
-        
-        
-        //add(labelLogHeroAction);
-        add(scrollTextAreaLogHeroAction);
-    }
+	public LogHeroActionPanel() {
+		setLayout(null);
+		// labelLogHeroAction = new JLabel("logoLog");
+		// labelLogHeroAction.setBounds(5, 5, 150, 50);
 
-    
-    
+		textAreaLogHeroAction = new JTextArea();
+		scrollTextAreaLogHeroAction = new JScrollPane(textAreaLogHeroAction);
 
-    public void setTextAreaLogHeroAction(String textAreaLog)
-    {
-             
-       // textAreaLogHeroAction.setText(textAreaLog);
-        textAreaLogHeroAction.append(textAreaLog+"\n");
-        textAreaLogHeroAction.setCaretPosition(textAreaLogHeroAction.getDocument().getLength());
-    }
+//		scrollTextAreaLogHeroAction.setBounds(5, 5, 170, 385);
+
+		// test filler
+		String str = "the game begins \n";
+
+		textAreaLogHeroAction.setText(str);
+
+		// add(labelLogHeroAction);
+		add(scrollTextAreaLogHeroAction);
+
+		addComponentListener(new ComponentAdapter() {
+
+			@Override
+			public void componentResized(ComponentEvent e) {
+				scrollTextAreaLogHeroAction.setBounds(5, 5, getWidth() - 10, getHeight() - 10);
+
+			}
+
+		});
+	}
+
+	public void setTextAreaLogHeroAction(String textAreaLog) {
+
+		// textAreaLogHeroAction.setText(textAreaLog);
+		textAreaLogHeroAction.append(textAreaLog + "\n");
+		textAreaLogHeroAction.setCaretPosition(textAreaLogHeroAction.getDocument().getLength());
+	}
 
 }
