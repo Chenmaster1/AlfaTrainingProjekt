1ee675bb084cf3b7da82d426823acddad9c09bed
Alfa <Alfa@K-Home-005>
2020-03-20 09:21:58 +0100

    Friedhof Sudden Death

--- AlfaTrainingProject/Logic/GameLogic/SingleplayerGame.java
+++ AlfaTrainingProject/Logic/GameLogic/SingleplayerGame.java
@@ -694,7 +694,7 @@ public class SingleplayerGame implements HeroEventListener {
 			h.setAttackable(true);
 
 		}
-
+                SoundController.playSound("Sudden_Death.mp3");
 		JOptionPane.showMessageDialog(mainFrame, MyFrame.bundle.getString("suddenDeathAnnounced"));
 	}
 
